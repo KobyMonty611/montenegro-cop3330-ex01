16074e40d2ea699198354282a02873c56a95a158
KobyMonty611 <kobymonty611@knights.ucf.edu>
2021-09-13 08:44:03 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 
+ */
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    System.out.print("What is your name? ");
+    Scanner scanner = new Scanner(System.in);
+    String name = scanner.nextLine();
+    System.out.println("Hello, " + name + ", nice to meet you!");
+    scanner.close();
+  }
+}
